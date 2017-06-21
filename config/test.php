9b92b83db434209de866335e185139b79a859ed7
Vadim Mazur <mazurva@gmail.com>
2017-06-21 15:15:59 +0600

    Update test.php

--- config/test.php
+++ config/test.php
@@ -1,6 +1,9 @@
 <?php
 $params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/test_db.php');
+$view = require(__DIR__ . '/view.php');
+
+$modules = array_merge([], require(__DIR__ . '/modules.php'));
 
 /**
  * Application configuration shared by all test types
@@ -9,6 +12,11 @@ return [
     'id' => 'basic-tests',
     'basePath' => dirname(__DIR__),    
     'language' => 'en-US',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'modules' => $modules,
     'components' => [
         'db' => $db,
         'mailer' => [
@@ -32,7 +40,8 @@ return [
                 'domain' => 'localhost',
             ],
             */
-        ],        
+        ],    
+        'view' => $view,    
     ],
     'params' => $params,
 ];
